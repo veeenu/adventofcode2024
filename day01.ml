791de4f6b0ec57eafd604dff2f9405cc14982745
Andrea Venuta <venutawebdesign@gmail.com>
2024-12-01 23:30:40 +0100

    Day 01 a little less hideous

--- day01.ml
+++ day01.ml
@@ -1,49 +1,48 @@
-let lines : string list =
-  let fp = open_in "_input/day01.txt" in
-  let rec read_lines acc =
-    try
-      let line = input_line fp in
-      read_lines (line :: acc)
-    with End_of_file -> List.rev acc
-  in
-  read_lines []
+open Common
 
-let parse line : (int * int) option =
-  match String.split_on_char ' ' line |> List.filter (fun x -> x <> "") with
-  | [ fst; snd ] ->
-      Some (String.trim fst |> int_of_string, String.trim snd |> int_of_string)
+let parse line =
+  String.split_on_char ' ' line
+  |> List.filter_map (function
+       | "" -> None
+       | x -> Some (String.trim x |> int_of_string))
+  |> function
+  | [ x1; x2 ] -> Some (x1, x2)
   | [] -> None
   | _ -> raise (Failure (Printf.sprintf "Invalid line `%s`" line))
 
-let numbers = List.map parse lines |> List.filter_map (fun x -> x)
-let list1 = List.map fst numbers |> List.sort compare
-let list2 = List.map snd numbers |> List.sort compare
+let list1, list2 =
+  read_day_lines 1 |> List.filter_map parse
+  |> List.fold_left
+       (fun (acc1, acc2) (x1, x2) -> (x1 :: acc1, x2 :: acc2))
+       ([], [])
+  |> fun (list1, list2) -> (List.sort compare list1, List.sort compare list2)
 
 (* Part 1 *)
 
 let part1 =
   List.combine list1 list2
   |> List.map (fun (a, b) -> b - a |> abs)
+  |> List.map (fun x ->
+         Printf.printf "%d\n" x;
+         x)
   |> List.fold_left ( + ) 0
 
 let () = Printf.printf "Part1: %d\n" part1
 
 (* Part 2 *)
 
-let incmap map value =
-  Hashtbl.find_opt map value |> Option.value ~default:0
-  |> (fun x -> x + 1)
-  |> Hashtbl.replace map value;
-  map
-
 let occurrences =
-  List.fold_left incmap (Hashtbl.create (List.length list2)) list2
+  List.fold_left
+    (fun map key ->
+      Hashtbl.find_opt map key
+      |> (function Some x -> x + 1 | None -> 1)
+      |> Hashtbl.replace map key;
+      map)
+    (Hashtbl.create (List.length list2))
+    list2
 
 let score_fn x =
-  Hashtbl.find_opt occurrences x
-  |> Option.map (fun y -> y * x)
-  |> Option.value ~default:0
+  Hashtbl.find_opt occurrences x |> function Some y -> y * x | None -> 0
 
-let score = List.map score_fn list1 |> List.fold_left ( + ) 0
-
-let () = Printf.printf "Part2: %d\n" score
+let part2 = List.map score_fn list1 |> List.fold_left ( + ) 0
+let () = Printf.printf "Part2: %d\n" part2
