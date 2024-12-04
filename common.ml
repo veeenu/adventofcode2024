fe91b6dcd6dc07e8e3b884b9ebbcae8a933bf7be
Andrea Venuta <venutawebdesign@gmail.com>
2024-12-04 11:48:11 +0100

    Day 04 part 2

--- common.ml
+++ common.ml
@@ -1,9 +1,10 @@
-let read_day_lines day: string list =
+let read_day_lines day : string list =
   let fp = Printf.sprintf "_input/day%02d.txt" day |> open_in in
   let lines = In_channel.input_lines fp in
-  close_in fp; lines
+  close_in fp;
+  lines
 
-let split_lines string =
-  string |> String.trim |> String.split_on_char '\n'
-
+let split_lines string = string |> String.trim |> String.split_on_char '\n'
 let to_char_ll l = l |> List.map (fun x -> x |> String.to_seq |> List.of_seq)
+let range_up start count = List.init count (fun i -> start + i)
+let range_dn start count = List.init count (fun i -> start - i)
--- day04.ml
+++ day04.ml
@@ -27,8 +27,6 @@ struct
   let char_at x y = List.nth (List.nth input y) x
   let w = List.length (List.nth input 0)
   let h = List.length input
-  let range_up start count = List.init count (fun i -> start + i)
-  let range_dn start count = List.init count (fun i -> start - i)
   let dir_n y = if y >= 3 then Some (range_dn y 4) else None
   let dir_s y = if y <= h - 4 then Some (range_up y 4) else None
   let dir_w x = if x >= 3 then Some (range_dn x 4) else None
@@ -61,18 +59,40 @@ struct
       (range_up 0 w)
 
   let count_xmas_at (x, y) =
-    List.fold_left ( + ) 0
-      (List.filter_map
-         (fun fn -> fn x y)
-         [ case_n; case_s; case_e; case_w; case_ne; case_nw; case_se; case_sw ]
-      |> List.map is_xmas
-      |> List.map (function true -> 1 | false -> 0))
+    List.filter_map
+      (fun fn -> fn x y)
+      [ case_n; case_s; case_e; case_w; case_ne; case_nw; case_se; case_sw ]
+    |> List.filter is_xmas
+    |> List.map (fun _ -> 1)
+    |> List.fold_left ( + ) 0
 
   let part1 =
     List.fold_left ( + ) 0
       (indices
       |> List.filter (fun (x, y) -> char_at x y = 'X')
       |> List.map count_xmas_at)
+
+  let neighbors =
+    [
+      [ 'M'; 'M'; 'S'; 'S' ];
+      [ 'S'; 'S'; 'M'; 'M' ];
+      [ 'M'; 'S'; 'M'; 'S' ];
+      [ 'S'; 'M'; 'S'; 'M' ];
+    ]
+    |> List.map (fun l -> List.combine [ (-1, -1); (1, -1); (-1, 1); (1, 1) ] l)
+
+  let check_neighbor x y l =
+    List.for_all (fun ((dx, dy), c) -> char_at (x + dx) (y + dy) == c) l
+
+  let check_neighborhood (x, y) = List.exists (check_neighbor x y) neighbors
+
+  let part2 =
+    indices
+    |> List.filter (fun (x, y) -> x >= 1 && y >= 1 && x < w - 1 && y < h - 1)
+    |> List.filter (fun (x, y) -> char_at x y == 'A')
+    |> List.filter check_neighborhood
+    |> List.map (fun _ -> 1)
+    |> List.fold_left ( + ) 0
 end
 
 module TestCase = Day04 (struct
@@ -85,3 +105,5 @@ end)
 
 let () = assert (TestCase.part1 == 18)
 let () = printf "Part 1: %d\n" Input.part1
+let () = assert (TestCase.part2 == 9)
+let () = printf "Part 2: %d\n" Input.part2
