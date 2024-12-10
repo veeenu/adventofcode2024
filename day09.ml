3b41e70903780431f175f7580ba070f22d3003e1
Andrea Venuta <venutawebdesign@gmail.com>
2024-12-10 11:14:03 +0100

    Day 09 part 2

--- day09.ml
+++ day09.ml
@@ -53,11 +53,8 @@ let part1 input =
     else n
   in
 
-  let () = dbg fs in
-
   let rec swap_loop i n =
     if i < n then
-      let () = printf "%d %d\n" i n in
       match fs.(i) with
       | File _ -> swap_loop (i + 1) n
       | Empty -> swap_loop (i + 1) (swap_last_with i n)
@@ -66,26 +63,33 @@ let part1 input =
 
   let () = swap_loop 0 (Array.length fs - 1) in
 
-  let () = dbg fs in
-
   checksum fs
 
 let part2 input =
   let fs = input |> List.mapi (fun id (file, space) -> (id, file, space)) in
   let rfs = List.rev fs in
 
-  let swap_slots fs id (rid, rfile, rspace) =
+  let rec take_while pred l =
+    match l with
+    | [] -> []
+    | hd :: tl -> if pred hd then hd :: take_while pred tl else []
+  in
+
+  let swap_slots fs id (rid, rfile, _) =
     fs
-    |> List.map (fun (vid, vfile, vspace) ->
-           if vid = id then [ (vid, vfile, 0); (rid, rfile, vspace - rfile) ]
-           else if vid = rid then [ (rid, 0, rfile + rspace) ]
-           else [ (vid, vfile, vspace) ])
-    |> List.flatten
+    |> List.fold_left
+         (fun (found, nfs) (vid, vfile, vspace) ->
+           if vid = id && not found then
+             (true, (rid, rfile, vspace - rfile) :: (vid, vfile, 0) :: nfs)
+           else if vid = rid then (found, (rid, 0, rfile + vspace) :: nfs)
+           else (found, (vid, vfile, vspace) :: nfs))
+         (false, [])
+    |> snd |> List.rev
   in
 
   let find_slot fs rid rfile =
     fs
-    |> List.filter (fun (id, _, _) -> id <= rid)
+    |> take_while (fun (id, _, _) -> id != rid)
     |> List.find_opt (fun (_, _, space) -> space >= rfile)
     |> Option.map (fun (id, _, _) -> id)
   in
@@ -97,28 +101,14 @@ let part2 input =
   in
 
   let rec defrag rev_fs fs =
-    match rev_fs with
-    | [] -> fs
-    | hd :: tl ->
-        try_swap_slots fs hd
-        |> inspect_one (fun xs ->
-               printf "[%d]: " (hd |> fun (x, _, _) -> x);
-               xs
-               |> List.map (fun (id, file, space) -> expand id (file, space))
-               |> List.flatten |> Array.of_list |> dbg)
-        |> defrag tl
+    match rev_fs with [] -> fs | hd :: tl -> try_swap_slots fs hd |> defrag tl
   in
 
-  let new_fs =
-    defrag rfs fs
-    |> List.map (fun (id, file, space) -> expand id (file, space))
-    |> List.flatten |> Array.of_list
-  in
-  let () = dbg new_fs in
-
-  checksum new_fs
+  defrag rfs fs
+  |> List.map (fun (id, file, space) -> expand id (file, space))
+  |> List.flatten |> Array.of_list |> checksum
 
 let () = printf "Test 1: %d\n" (parse test_case |> part1)
 let () = printf "Part 1: %d\n" (parse (read_day_lines 9 |> List.hd) |> part1)
 let () = printf "Test 2: %d\n" (parse test_case |> part2)
-(* let () = printf "Part 2: %d\n" (parse (read_day_lines 9 |> List.hd) |> part2) *)
+let () = printf "Part 2: %d\n" (parse (read_day_lines 9 |> List.hd) |> part2)
