c0e5f07a197cee844bcdb386a32e22a2897a2343
Andrea Venuta <venutawebdesign@gmail.com>
2024-12-19 13:51:14 +0100

    Day 19 part 2

--- day19.ml
+++ day19.ml
@@ -30,7 +30,8 @@ let parse input =
   in
   (towels, designs)
 
-let rec is_prefix design_block towel = match (design_block, towel) with
+let rec is_prefix design_block towel =
+  match (design_block, towel) with
   | ds, [] -> Some ds
   | [], _ -> None
   | d :: ds, t :: ts -> if d = t then is_prefix ds ts else None
@@ -48,30 +49,18 @@ let find_a_combination (towels : char list list) (design : char list) =
 
   search [ design ]
 
-(* let count_combinations = Memo.make (fun design ->  *)
-(**)
-(* ) *)
+let count_combinations (towels : char list list) (design : char list) =
+  let neighbors design = towels |> List.filter_map (is_prefix design) in
+  let search_memo = Memo.empty () in
 
-(* let find_combinations (towels : char list list) (design : char list) = *)
-(*   let find_design_prefixes design = *)
-(*     Memo.memo prefixes_memo design (fun design -> *)
-(*         towels *)
-(*         |> List.filter_map (fun towel -> *)
-(*                is_prefix design towel *)
-(*                |> Option.map (fun block -> (towel, block)))) *)
-(*   in *)
-(**)
-(*   let rec find_combinations_inner design : char list list list = *)
-(*     if List.is_empty design then [ [] ] *)
-(*     else *)
-(*       find_design_prefixes design *)
-(*       |> List.map (fun (towel, block) -> *)
-(*              find_combinations_inner block *)
-(*              |> List.map (fun next_combinations -> towel :: next_combinations)) *)
-(*       |> List.flatten *)
-(*   in *)
-(**)
-(*   find_combinations_inner design |> List.filter (List.is_empty >> not) *)
+  let rec search = function
+    | [] -> 1
+    | rest_design ->
+        Memo.memo search_memo rest_design (fun rest_design ->
+            neighbors rest_design |> List.map search |> List.fold_left ( + ) 0)
+  in
+
+  search design
 
 let print_combo (combo : char list list) =
   let _ = printf "Combo of length %d: " (List.length combo) in
@@ -80,10 +69,13 @@ let print_combo (combo : char list list) =
 
 let part1 input =
   let towels, designs = parse input in
-  (* |> List.map (fun design -> *)
-  (*        let _ = printf "\nDesign: %s%!\n" (cltos design) in *)
-  (*        find_combinations towels design |> inspect print_combo |> List.length) *)
   designs |> List.filter (find_a_combination towels) |> List.length
+
+let part2 input =
+  let towels, designs = parse input in
+  designs |> List.map (count_combinations towels) |> List.fold_left ( + ) 0
 
 let _ = part1 test_case |> printf "Test 1: %d\n"
 let _ = part1 day_input |> printf "Part 1: %d\n"
+let _ = part2 test_case |> printf "Test 2: %d\n"
+let _ = part2 day_input |> printf "Part 2: %d\n"
