1bfaf82f1501dbe1910e8e77ad4a0e9b6c06e45d
Andrea Venuta <venutawebdesign@gmail.com>
2024-12-23 14:53:04 +0100

    Day 23 part 2

--- day23.ml
+++ day23.ml
@@ -48,6 +48,12 @@ module ComputerMap = Map.Make (struct
 end)
 
 module ComputerSet = Set.Make (struct
+  type t = char * char
+
+  let compare = compare
+end)
+
+module ComputerTripleSet = Set.Make (struct
   type t = (char * char) * (char * char) * (char * char)
 
   let compare = compare
@@ -100,8 +106,67 @@ let triples g =
            |> Option.map (List.filter_map (fun v3 -> filter_triple g v1 v2 v3)))
     |> List.flatten
   in
-  triples |> ComputerSet.of_list |> ComputerSet.filter triple_has_t
+  triples |> ComputerTripleSet.of_list |> ComputerTripleSet.filter triple_has_t
 
-let part1 input = input |> parse |> graph |> triples |> ComputerSet.cardinal
+let part1 input =
+  input |> parse |> graph |> triples |> ComputerTripleSet.cardinal
+
 let _ = part1 test_case |> printf "Test 1: %d\n"
 let _ = part1 day_input |> printf "Part 1: %d\n"
+
+let bfs v g =
+  let rec aux visited = function
+    | [] -> []
+    | v1 :: q ->
+        let neighbors =
+          ComputerMap.find v1 g
+          |> List.filter (fun v2 -> not (ComputerSet.mem v2 visited))
+        in
+        v1 :: aux (ComputerSet.add v1 visited) (q @ neighbors)
+  in
+
+  aux ComputerSet.empty [ v ]
+
+let cliques g =
+  let vertices = ComputerMap.to_list g |> List.map fst in
+  let is_adjacent v1 v2 = ComputerMap.find v1 g |> List.mem v2 in
+  let rec find_clique v1 clique =
+    if List.mem v1 clique || not (List.for_all (is_adjacent v1) clique) then
+      clique
+    else
+      let neighbors =
+        ComputerMap.find v1 g
+        |> List.filter (fun v2 ->
+               (not (List.mem v2 clique))
+               && List.for_all (is_adjacent v2) clique
+               && is_adjacent v1 v2)
+      in
+      neighbors
+      |> List.fold_left
+           (fun acc neighbor -> find_clique neighbor acc)
+           (v1 :: clique)
+  in
+
+  let rec find_all_cliques unvisited cliques =
+    match unvisited with
+    | [] -> cliques
+    | v :: vs ->
+        let clique = find_clique v [] in
+        let visited = List.flatten cliques in
+        let vs = vs |> List.filter (fun v -> not (List.mem v visited)) in
+        find_all_cliques vs (clique :: cliques)
+  in
+  find_all_cliques vertices []
+
+let part2 input =
+  input |> parse |> graph |> cliques
+  |> List.fold_left
+       (fun acc component ->
+         if List.length acc < List.length component then component else acc)
+       []
+  |> List.sort_uniq compare
+  |> List.map (fun (va, vb) -> sprintf "%c%c" va vb)
+  |> String.concat ","
+
+let _ = part2 test_case |> printf "Test 2: %s\n"
+let _ = part2 day_input |> printf "Part 2: %s\n"
